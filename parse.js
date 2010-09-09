39e5cdda6de0515a8dcd9a5cb420ddf9da71ea47
infinte <infinte.cdda@hotmail.com>
2010-09-09 17:46:23 +0800

    Continuing for ELSE...

--- parse.js
+++ parse.js
@@ -758,13 +758,11 @@ return function (tokens) {
 	};
 
 	var contBlock = function (fin) {
-		var e = fin || END;
 		switch (token.type) {
 			case COLON:
 				advance();
 				var s = statements(fin);
-				if(tokenIs(END) && e!==END) throw new Error('The control block is wrongly ended by END. Maybe an ELSE after it?');
-				ensure(tokenIs(e), 'Unterminated statement block');
+				ensure(tokenIs(fin) || tokenIs(END), 'Unterminated statement block');
 				if (tokenIs(END)) advance();
 				return s;
 			case COMMA:
@@ -782,7 +780,7 @@ return function (tokens) {
 		var n = new Node(nt.IF);
 		n.condition = callItem();
 		n.thenPart = contBlock(ELSE);
-		while(tokenIs(SEMICOLON) && nextIs(ELSE)) advance();
+		if(tokenIs(SEMICOLON) && nextIs(END)) advance();
 		if (tokenIs(ELSE)) {
 			advance(ELSE);
 			if (token.type === IF) n.elsePart = ifstmt();
